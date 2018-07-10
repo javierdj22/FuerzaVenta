0df3a9fa0ea57dc419dcd8f6c9cb675ebc325904
javierdj22 <javierdj22@gmail.com>
2018-07-10 11:17:05 -0500

    Edit Delete
    Co-Authored-By: javy <javy@users.noreply.github.com>

--- src/components/Axios/Axios.jsx
+++ src/components/Axios/Axios.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 import axios from 'axios';
 import Table from "components/Table/Table.jsx";
@@ -12,39 +13,28 @@ import tasksStyle from "assets/jss/material-dashboard-react/components/tasksStyl
 import Edit from "@material-ui/icons/Edit";
 import Close from "@material-ui/icons/Close";
 import Check from "@material-ui/icons/Check";
-const styles = {
-    cardCategoryWhite: {
-      "&,& a,& a:hover,& a:focus": {
-        color: "rgba(255,255,255,.62)",
-        margin: "0",
-        fontSize: "14px",
-        marginTop: "0",
-        marginBottom: "0"
-      },
-      "& a,& a:hover,& a:focus": {
-        color: "#FFFFFF"
-      }
-    },
-    cardTitleWhite: {
-      color: "#FFFFFF",
-      marginTop: "0px",
-      minHeight: "auto",
-      fontWeight: "300",
-      fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-      marginBottom: "3px",
-      textDecoration: "none",
-      "& small": {
-        color: "#777",
-        fontSize: "65%",
-        fontWeight: "400",
-        lineHeight: "1"
-      }
+
+class PersonList extends React.Component {
+    state = {
+      checked: this.props.checkedIndexes,
+      persons: []
+    };
+    
+  handleToggle = persons => () => {
+    const { checked } = this.state;
+    const currentIndex = checked.indexOf(persons.Codigo);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(persons.Codigo);
+    } else {
+      newChecked.splice(currentIndex, 1);
     }
+
+    this.setState({
+      checked: newChecked
+    });
   };
-export default class PersonList extends React.Component {
-  state = {
-    persons: []
-  }
 
   componentDidMount() {
     var config = {
@@ -76,28 +66,58 @@ export default class PersonList extends React.Component {
             tableData={
             this.state.persons.map(persons =>[persons.Nombre, persons.Codigo, persons.FechaRegistro, 
             <Checkbox
-                checked={persons.Activo}
-                tabIndex={persons.Codigo}
-                //onClick={this.handleToggle(persons.Activo)}
-                checkedIcon={<Check className={[persons.Codigo]} />}
-                icon={<Check className={[persons.Codigo]} />}
-                //classes={{
-                //    checked: classes.checked
-                //}}
-            /> 
-            /*
-            <Checkbox
-              checked={this.state.checked.indexOf(value) !== -1}
+              checked={persons.Activo}
               tabIndex={-1}
-              onClick={this.handleToggle(value)}
+              onClick={this.handleToggle(persons.Codigo)}
               checkedIcon={<Check className={classes.checkedIcon} />}
               icon={<Check className={classes.uncheckedIcon} />}
               classes={{
                 checked: classes.checked
               }}
-            />*/
+            />,
+            <Tooltip
+              id="tooltip-top"
+              title="Edit Task"
+              placement="top"
+              classes={{ tooltip: classes.tooltip }}
+            >
+              <IconButton
+                aria-label="Edit"
+                className={classes.tableActionButton}
+              >
+                <Edit
+                  className={
+                    classes.tableActionButtonIcon + " " + classes.edit
+                  }
+                />
+              </IconButton>
+            </Tooltip>,
+            <Tooltip
+                id="tooltip-top-start"
+                title="Remove"
+                placement="top"
+                classes={{ tooltip: classes.tooltip }}
+            >
+                <IconButton
+                aria-label="Close"
+                className={classes.tableActionButton}
+                >
+                <Close
+                    className={
+                    classes.tableActionButtonIcon + " " + classes.close
+                    }
+                />
+                </IconButton>
+            </Tooltip>
             ])}
         />
     )
   }
 }
+PersonList.propTypes = {
+  classes: PropTypes.object.isRequired,
+  tasksIndexes: PropTypes.arrayOf(PropTypes.number),
+  PersonList: PropTypes.arrayOf(PropTypes.node)
+};
+
+export default withStyles(tasksStyle)(PersonList);
